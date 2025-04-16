bf5113cfbb57ba91c01ff142f00f606f8fc7fde5
Faheem Malik <faheemmalik3000@gmail.com>
2025-04-16 11:35:06 +0500

    UI has been created for add task

---
+++ scripts/main.js
@@ -0,0 +1,9 @@
+
+const addTaskModal = document.getElementById("addTaskModal")
+
+const showAddTaskModal =()=>{
+    addTaskModal.style.display="flex"
+}
+const closeAddTaskModal=()=>{
+    addTaskModal.style.display="none"
+}
